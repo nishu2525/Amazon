99503c320e099f7f1145ee2a417810e69a7836ac
Sare@20 <umapnishad1@gmail.com>
2024-06-26 22:21:17 +0530

    Setup Ready

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import Navbar from './components/Header/Navbar';
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+   </>
+  );
+}
+
+export default App;
